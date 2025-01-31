5d7fc3ac87ec044173af3a21736b26077f984cef
MiguelTEARCI <miguelolx19@sapo.pt>
2025-01-31 19:16:28 +0000

    Add files via upload

---
+++ Arduino_LED_Blink_V4.ino
@@ -0,0 +1,25 @@
+void setup() {
+  // put your setup code here, to run once:
+  pinMode(12,OUTPUT);
+  pinMode(8,OUTPUT);
+  pinMode(7,OUTPUT);
+
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  digitalWrite(12,HIGH);
+  delay(2000);
+  digitalWrite(12,LOW);
+  delay(2000);
+  digitalWrite(8,HIGH);
+  delay(2000);
+  digitalWrite(8,LOW);
+  delay(2000);
+  digitalWrite(7,HIGH);
+  delay(2000);
+  digitalWrite(7,LOW);
+  delay(2000);
+
+}
